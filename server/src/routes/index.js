2183c1a6f2ca0824a718afa42f2600f811cc228d
Lisandro <lisandroviedo@hotmail.com>
2023-12-14 13:44:11 -0400

    Drivers v1.1.1

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,4 +2,23 @@ const { Router } = require("express");
 
 const router = Router();
 
+router.get("/drivers", (req, res) => {
+  return res.json({ message: "Funciona get /drivers" });
+});
+router.get("/drivers/:idDriver", (req, res) => {
+  const { idDriver } = req.params;
+  return res.json({ message: `Funciona get /drivers/${idDriver}` });
+});
+router.get("/drivers/name?", (req, res) => {
+  return res.json({ message: "Funciona get /drivers/name?" });
+});
+
+router.post("/drivers", (req, res) => {
+  return res.json({ message: "Funciona post /drivers" });
+});
+
+router.get("/teams", (req, res) => {
+  return res.json({ message: "Funciona get /teams" });
+});
+
 module.exports = router;
