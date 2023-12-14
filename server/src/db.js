ea815e4d6bd6fb680212db5e2c8ef00af73202dd
Lisandro <lisandroviedo@hotmail.com>
2023-12-14 12:34:37 -0400

    Drivers v1.1.0

--- server/src/db.js
+++ server/src/db.js
@@ -34,7 +34,9 @@ let capsEntries = entries.map((entry) => [
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Driver } = sequelize.models;
+const { Driver, Team } = sequelize.models;
+Driver.belongsToMany(Team, { through: "driver_team" });
+Team.belongsToMany(Driver, { through: "driver_team" });
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
@@ -42,4 +44,6 @@ const { Driver } = sequelize.models;
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  Driver,
+  Team,
 };
--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,11 +1,36 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Driver', {
+  sequelize.define("Driver", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthdate: {
+      type: DataTypes.DATEONLY,
       allowNull: false,
     },
   });
