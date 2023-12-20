ce22083e5af93b1a6d361661c63d5041e89679a3
lisandroviedo <lisandroviedo@hotmail.com>
2023-12-20 17:27:10 -0400

    Drivers 1.1.5

--- server/src/controllers/postDriver.js
+++ server/src/controllers/postDriver.js
@@ -1,49 +1,31 @@
-const { Driver } = require("../db");
+const { Driver, Team } = require("../db");
 
 const postDriver = async (req, res) => {
   try {
-    const { id, name, last_name, description, image, nationality, birthdate } =
-      req.body;
+    const {
+      name,
+      last_name,
+      description,
+      image,
+      nationality,
+      birthdate,
+      teams,
+    } = req.body;
 
-    // const [driver, created] = await Driver.findOrCreate({
-    //   where: { id: id },
-    //   defaults: {
-    //     id: id,
-    //     name: name,
-    //     last_name: last_name,
-    //     description: description,
-    //     image: image,
-    //     nationality: nationality,
-    //     birthdate: birthdate,
-    //   },
-    // });
+    if (name && last_name && description && image && nationality && birthdate) {
+      const driver = await Driver.create({
+        name: name,
+        last_name: last_name,
+        description: description,
+        image: image,
+        nationality: nationality,
+        birthdate: birthdate,
+      });
 
-    const conductor = {
-      id: id,
-      name: name,
-      last_name: last_name,
-      description: description,
-      image: image,
-      nationality: nationality,
-      birthdate: birthdate,
-    };
-
-    await Driver.create(
-      {
-        id: 1,
-        title: "Chair",
-        tags: [{ name: "Alpha" }, { name: "Beta" }],
-      },
-      {
-        include: [Tag],
-      }
-    );
-
-    if (!created) {
-      return res.status(400).json({ error: "El driver no pudo registrarse" });
+      return res.status(201).json(driver);
     }
 
-    return res.status(201).json(driver);
+    return res.status(400).json({ error: "Faltan datos" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
