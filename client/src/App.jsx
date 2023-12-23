27a03e1e13e97373d148cf5e0e6bbef1f22f1430
lisandroviedo <lisandroviedo@hotmail.com>
2023-12-23 18:25:16 -0400

    Drivers v1.3.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,18 @@ import axios from "axios";
 import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { useState, useMemo } from "react";
 
+import AboutMe from "./components/AboutMe/AboutMe";
+import Driver from "./components/Driver/Driver";
+import DriverDetails from "./components/DriverDetails/DriverDetails";
+import Drivers from "./components/Drivers/Drivers";
+import Landing from "./components/Landing/Landing";
+import Login from "./components/Login/Login";
+import NavBar from "./components/NavBar/NavBar";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import RegisterDriver from "./components/RegisterDriver/RegisterDriver";
+import RegisterUser from "./components/RegisterUser/RegisterUser";
+import SearchBar from "./components/SearchBar/SearchBar";
+
 function App() {
   return (
     <div>
