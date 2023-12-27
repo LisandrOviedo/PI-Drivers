995890aed4cb0a5e137445f0ea562335ff0ace75
lisandroviedo <lisandroviedo@hotmail.com>
2023-12-27 19:26:26 -0400

    Drivers v1.4.3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,23 @@ import RegisterUser from "./components/RegisterUser/RegisterUser";
 
 function App() {
   const [access, setAccess] = useState({ access: false });
+  const [drivers, setDrivers] = useState([]);
   const navigate = useNavigate();
   const URL_SERVER = import.meta.env.VITE_URL_SERVER;
+
+  async function searchAll() {
+    const URL_SEARCHALL = `${URL_SERVER}/`;
+
+    try {
+      const { data } = await axios(URL_SEARCHALL);
+
+      if (data[0].name) {
+        setDrivers(data);
+      }
+    } catch (error) {
+      window.alert(error.response.data.error);
+    }
+  }
 
   async function login({ email, password }) {
     const URL_LOGIN = `${URL_SERVER}/login/?email=${email.toLowerCase()}&password=${password}`;
@@ -56,14 +71,14 @@ function App() {
 
   function logout() {
     setAccess({ access: false });
-    navigate("/login");
+    alert("See you later! We hope to see you again soon!");
   }
 
-  useMemo(() => {
-    !access.access && navigate("/");
-  }, [access]);
+  useMemo(async () => {
+    access.access && searchAll();
 
-  const [drivers, setDrivers] = useState([]);
+    !access.access && navigate("/login");
+  }, [access]);
 
   async function onSearch(name) {
     const URL_SEARCH = `${URL_SERVER}/drivers?name=${name}`;
